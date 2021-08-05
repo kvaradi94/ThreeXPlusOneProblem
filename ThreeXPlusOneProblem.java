178c6461edfb04adedfd696af490b63ec1a0f7ef
Kálmán Váradi <k.varadi94@gmail.com>
2021-08-05 16:52:30 +0200

    add interval test

--- ThreeXPlusOneProblem.java
+++ ThreeXPlusOneProblem.java
@@ -16,22 +16,28 @@ public class ThreeXPlusOneProblem {
     private static final BigInteger TWO = new BigInteger("2");
     private static final BigInteger THREE = new BigInteger("3");
     private static final int MULTIPLIER_FOR_MAXIMUM_TIMEOUT = 100;
-    private static final double DEVIDE_NANO_TO_GET_SEC = 1_000_000_000;
-    private static final String START_NUMBER = "1";
-    private static final String END_NUMBER = "10000";
-    private static final BigInteger STEP = new BigInteger("1000");
+    private static final double DIVIDE_NANO_TO_GET_SEC = 1_000_000_000;
+    private static String startNumber = "1";
+    private static String endNumber = "10000";
+    private static BigInteger step;
 
     public static void main(String[] args) {
-        if(args.length != 0) {
+        if (args.length == 1) {
             testArgNumber(args[0]);
-            System.out.println("Max of the series: " +  Collections.max(series));
+            System.out.println("Max of the series: " + Collections.max(series));
             System.out.println("Size of the series: " + series.size());
             return;
         }
-        final BigInteger startNumber = new BigInteger(START_NUMBER);
-        final BigInteger endNumber = new BigInteger(END_NUMBER);
+        if (args.length == 2) {
+            startNumber = args[0];
+            endNumber = args[1];
+        }
 
-        for (BigInteger i = startNumber; isLess(i, endNumber); i = i.add(BigInteger.ONE)) {//NOPMD
+        final BigInteger startNumber = new BigInteger(ThreeXPlusOneProblem.startNumber);
+        final BigInteger endNumber = new BigInteger(ThreeXPlusOneProblem.endNumber);
+        step = calculateStep(startNumber, endNumber);
+
+        for (BigInteger i = startNumber; isLessOrEqual(i, endNumber); i = i.add(BigInteger.ONE)) {
             startTime = System.nanoTime();
             if (searchCounterExample(i)) {
                 System.out.println("Possible counter example: ");
@@ -41,21 +47,21 @@ public class ThreeXPlusOneProblem {
             final long endTime = System.nanoTime();
             final long duration = endTime - startTime;
             executionTimes.add(duration);
-            if (divisible(i, STEP)) {
-                System.out.println("Progress: " + i + " from " + END_NUMBER);
-                System.out.println("Max of maximums: " + Collections.max(maximums));
-                System.out.println("Max of lengths: " + Collections.max(lengths));
-                System.out.println("Max of execution times in sec: " + Collections.max(executionTimes) / DEVIDE_NANO_TO_GET_SEC);
-            }
             maximums.add(Collections.max(series));
             lengths.add(series.size());
+            if (divisible(i, step)) {
+                System.out.println("Progress: " + i + " from " + ThreeXPlusOneProblem.endNumber);
+                System.out.println("Max of maximums: " + Collections.max(maximums));
+                System.out.println("Max of lengths: " + Collections.max(lengths));
+                System.out.println("Max of execution times in sec: " + Collections.max(executionTimes) / DIVIDE_NANO_TO_GET_SEC);
+            }
             series.clear();
             firstIteration = false;
         }
     }
 
     private static boolean searchCounterExample(BigInteger inputNumber) {
-        if(isTheProcessingTakesTooLong()) {
+        if (isTheProcessingTakesTooLong()) {
             return true;
         }
         series.add(inputNumber);
@@ -70,10 +76,10 @@ public class ThreeXPlusOneProblem {
     }
 
     private static boolean searchCounterExample(BigInteger inputNumber, boolean testArgNumberFlag) {
-        if(isTheProcessingTakesTooLong()) {
+        if (isTheProcessingTakesTooLong()) {
             return true;
         }
-        if((boolean) testArgNumberFlag) {
+        if (testArgNumberFlag) {
             System.out.println(inputNumber);
         }
         series.add(inputNumber);
@@ -92,19 +98,31 @@ public class ThreeXPlusOneProblem {
         searchCounterExample(number, true);
     }
 
-    private static boolean isTheProcessingTakesTooLong()  {
-        if(!firstIteration) {
+    private static boolean isTheProcessingTakesTooLong() {
+        if (!firstIteration) {
             final long currentTime = System.nanoTime();
             final long currentProcessTimeOnSpecificNumber = currentTime - startTime;
-            if(currentProcessTimeOnSpecificNumber > Collections.max(executionTimes)* MULTIPLIER_FOR_MAXIMUM_TIMEOUT) {
+            if (currentProcessTimeOnSpecificNumber > Collections.max(executionTimes) * MULTIPLIER_FOR_MAXIMUM_TIMEOUT) {
                 return true;
             }
         }
         return false;
     }
 
+    private static BigInteger calculateStep(BigInteger start, BigInteger end) {
+        BigInteger diffOfStartAndEnd = subtract(end, start);
+        if (isLess(diffOfStartAndEnd, BigInteger.TEN)) {
+            return BigInteger.ONE;
+        }
+        return divide(diffOfStartAndEnd, BigInteger.TEN);
+    }
+
     private static boolean isLess(BigInteger firstNumber, BigInteger secondNumber) {
         return firstNumber.compareTo(secondNumber) == -1;
+    }
+
+    private static boolean isLessOrEqual(BigInteger firstNumber, BigInteger secondNumber) {
+        return firstNumber.compareTo(secondNumber) == -1 || firstNumber.compareTo(secondNumber) == 0;
     }
 
     private static boolean divisible(BigInteger firstNumber, BigInteger secondNumber) {
@@ -117,5 +135,9 @@ public class ThreeXPlusOneProblem {
 
     private static BigInteger multipleThreeThenAddOne(BigInteger number) {
         return number.multiply(THREE).add(ONE);
+    }
+
+    private static BigInteger subtract(BigInteger firstNumber, BigInteger secondNumber) {
+        return firstNumber.subtract(secondNumber);
     }
 }
