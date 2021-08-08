b8bd84b1fb1241f304731404a3dad70f173079c0
Kalman Varadi <k.varadi94@gmail.com>
2021-08-08 14:32:21 +0200

    print series stats as axis label

--- threexplusone/src/main/java/com/kvaradi/app/ChartGenerator.java
+++ threexplusone/src/main/java/com/kvaradi/app/ChartGenerator.java
@@ -7,29 +7,32 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
+import java.util.Collections;
+
 public class ChartGenerator extends ApplicationFrame {
 
     private final String firstNumber;
+    final Calculation calculation = new Calculation();
 
     public ChartGenerator(String applicationTitle, String chartTitle, String firstNumber) {
         super(applicationTitle);
         this.firstNumber = firstNumber;
+        calculation.testArgNumber(firstNumber);
+        final String categoryAxisLabel = "Max of the series: " + Collections.max(calculation.getSeries()) +
+                ", Size of the series: " + calculation.getSeries().size();
         JFreeChart lineChart = ChartFactory.createLineChart(
                 chartTitle,
-                "Iteration", "Current value",
+                categoryAxisLabel, "Current value",
                 createDataset(),
                 PlotOrientation.VERTICAL,
                 true, true, false);
 
         ChartPanel chartPanel = new ChartPanel(lineChart);
-        ///chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
         setContentPane(chartPanel);
     }
 
     public DefaultCategoryDataset createDataset() {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        final Calculation calculation = new Calculation();
-        calculation.testArgNumber(firstNumber);
         for (int i = 0; i < calculation.getSeries().size(); i++) {
             dataset.addValue(calculation.getSeries().get(i), "values", i);
         }
